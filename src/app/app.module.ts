a2cfceea97ecbd1f09c2865325569ced9eea06b7
Damaiceno Castro <damaiceno.castro@portmaputo.com>
2023-12-29 15:29:06 +0200

    Adding a home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from "@angular/forms";
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import { HomeComponent } from './home/home.component';
 //import { NavSkipTestsComponent } from "./nav --skip-tests/nav --skip-tests.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent//,
+    NavComponent,
+    HomeComponent//,
     //NavSkipTestsComponent
   ],
   imports: [
