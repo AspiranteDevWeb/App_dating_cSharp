f5860ca61c4ddda331acc448c61918389c7687b3
Damaiceno Castro <damaiceno.castro@portmaputo.com>
2024-01-22 16:45:37 +0200

    Reactive forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AccountService} from "../_service/account.service";
 import {error} from "@angular/compiler-cli/src/transformers/util";
 import {ToastrService} from "ngx-toastr";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -12,15 +13,25 @@ export class RegisterComponent implements OnInit{
  // @Input() usersFromHomeComponent:any;
   @Output() cancelRegister = new EventEmitter();
   model:any={}
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService) {
   }
 
   ngOnInit() {
+    this.initializeForm();
   }
 
+  initializeForm(){
+    this.registerForm= new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    })
+  }
   register(){
-    this.accountService.register(this.model).subscribe({
+    console.log(this.registerForm?.value);
+   /* this.accountService.register(this.model).subscribe({
       next: () => {
 
         this.cancel();
@@ -30,6 +41,7 @@ export class RegisterComponent implements OnInit{
           console.log(error)
       }
     })
+**/
 
   }
 
